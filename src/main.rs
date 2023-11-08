0b897da19bfe94995c4498d72ace3e90d6a89fc3
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2023-11-08 22:33:50 +0100

    Reading from binary files

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,146 @@
 use std::collections::HashMap;
+use std::io::{Cursor, Error as IoError, Read};
+use byteorder::{ReadBytesExt, BigEndian};
+
+const REG_A: u8 = 1;
+const REG_B: u8 = 2;
+const REG_C: u8 = 3;
+const REG_D: u8 = 4;
+const REG_E: u8 = 5;
+
+const INST_NOOP: u8 = 0;
+const INST_LOAD_CONSTANT: u8 = 1;
+const INST_ADD: u8 = 2;
+const INST_SUBTRACT: u8 = 3;
+const INST_COPY: u8 = 4;
+const INST_COMPARE: u8 = 5;
+const INST_JUMP: u8 = 6;
+const INST_JUMP_IF_NON_ZERO: u8 = 7;
+const INST_PRINT: u8 = 8;
 
 fn main() {
-    let mut emulator = Emulator::new();
-    emulator.run();
+    let args: Vec<String> = std::env::args().collect();
+    let file_name = args[1].trim();
+
+    let bytes = std::fs::read(file_name).expect("Failed to read file.");
+
+    let mut emulator = Emulator::new(bytes, false);
+    emulator.run().unwrap();
 }
 
+fn gen_file() {
 
-struct Emulator {
-    registries: HashMap<Registry, i32>,
-    instruction_pointer: usize,
-}
-
-impl Emulator {
-    pub fn new() -> Self {
-        Self {
-            registries: HashMap::new(),
-            instruction_pointer: 0,
-        }
-    }
-
-    pub fn run(&mut self) {
 //  0: A = 0
 //  1: B = 1
 //  2: C = A + B
 //  3: print(C)
 //  4: A = B
 //  5: B = C
-//  6: D = 100          }
+//  6: D = 65536        }
 //  7: D = C compare D  } C < 100
-//  8: E = -1           }
+//  8: E = 1            }
 //  9: E = D compare E  }
 // 10: jump if E != 0 (if E is "true")
 
-        let instructions = [
-            /*  0 */ Instruction::LoadConstant(Registry::A, 0),
-            /*  1 */ Instruction::LoadConstant(Registry::B, 1),
-            /*  2 */ Instruction::Add(Registry::A, Registry::B, Registry::C),
-            /*  3 */ Instruction::Print(Registry::C),
-            /*  4 */ Instruction::Copy(Registry::B, Registry::A),
-            /*  5 */ Instruction::Copy(Registry::C, Registry::B),
-            /*  6 */ Instruction::LoadConstant(Registry::D, 100),
-            /*  7 */ Instruction::Compare(Registry::C, Registry::D, Registry::D),
-            /*  8 */ Instruction::LoadConstant(Registry::E, -1),
-            /*  9 */ Instruction::Compare(Registry::D, Registry::E, Registry::E),
-            /* 10 */ Instruction::JumpIfNonZero(Registry::E, 2),
-        ];
+    let bytes = vec![
+        INST_LOAD_CONSTANT,     REG_A, 0, 0, 0, 0,
+        INST_LOAD_CONSTANT,     REG_B, 0, 0, 0, 1,
+        INST_ADD,               REG_A, REG_B, REG_C,
+        INST_PRINT,             REG_C,
+        INST_COPY,              REG_B, REG_A,
+        INST_COPY,              REG_C, REG_B,
+        INST_LOAD_CONSTANT,     REG_D, 0, 1, 0, 0,
+        INST_COMPARE,           REG_C, REG_D, REG_D,
+        INST_LOAD_CONSTANT,     REG_E, 0, 0, 0, 1,
+        INST_COMPARE,           REG_D, REG_E, REG_E,
+        INST_JUMP_IF_NON_ZERO,  REG_E, 0, 0, 0, 12
+    ];
 
+    std::fs::write("fibonacci.bin", bytes).unwrap();
+
+}
+
+#[derive(Debug)]
+pub enum EmulatorError {
+    FailedToRead(IoError),
+    InvalidInstruction(u8),
+    InvalidRegistry(u8),
+}
+
+impl From<IoError> for EmulatorError {
+    fn from(err: IoError) -> Self {
+        EmulatorError::FailedToRead(err)
+    }
+}
+
+struct Emulator {
+    bytes: Cursor<Vec<u8>>,
+    registries: HashMap<u8, i32>,
+    instruction_pointer: usize,
+    debug: bool,
+}
+
+impl Emulator {
+    pub fn new(bytes: Vec<u8>, debug: bool) -> Self {
+        Self {
+            bytes: Cursor::new(bytes),
+            registries: HashMap::new(),
+            instruction_pointer: 0,
+            debug
+        }
+    }
+
+    pub fn run(&mut self) -> Result<(), EmulatorError> {
         loop {
-            if self.instruction_pointer >= instructions.len() {
+            if self.bytes.position() >= self.bytes.get_ref().len() as u64 {
                 break;
             }
-            let inst = instructions[self.instruction_pointer];
+            let inst = self.bytes.read_u8()?;
+            if self.debug { println!("inst = {inst}"); }
             match inst {
-                Instruction::LoadConstant(reg, val) => {
-                    self.registry_set(reg, val);
+                INST_NOOP => {
+                    // no operation, do nothing
                 },
-                Instruction::Add(a, b, result) => {
+                INST_LOAD_CONSTANT => {
+                    let reg = self.read_registry()?;
+                    if self.debug { println!("reg = {reg:?}"); }
+                    let constant = self.bytes.read_i32::<BigEndian>()?;
+                    if self.debug { println!("constant = {constant}"); }
+
+                    self.registry_set(reg, constant);
+                }
+                INST_ADD => {
+                    let a = self.read_registry()?;
+                    let b = self.read_registry()?;
+                    let result = self.read_registry()?;
+
                     let a_val = self.registry_get(a);
                     let b_val = self.registry_get(b);
                     let result_val = a_val + b_val;
                     self.registry_set(result, result_val);
-                },
-                Instruction::Subtract(a, b, result) => {
+                }
+                INST_SUBTRACT => {
+                    let a = self.read_registry()?;
+                    let b = self.read_registry()?;
+                    let result = self.read_registry()?;
+
                     let a_val = self.registry_get(a);
                     let b_val = self.registry_get(b);
                     let result_val = a_val - b_val;
                     self.registry_set(result, result_val);
-                },
-                Instruction::Print(reg) => {
-                    let val = self.registry_get(reg);
-                    println!("{}", val);
-                },
-                Instruction::Jump(to) => {
-                    self.jump(to);
-                },
-                Instruction::JumpIfNonZero(reg, to) => {
-                    let val = self.registry_get(reg);
-                    if val == 0 {
-                        self.jump(to);
-                    }
-                },
-                Instruction::Copy(from, to) => {
+                }
+                INST_COPY => {
+                    let from = self.read_registry()?;
+                    let to = self.read_registry()?;
+
                     let val = self.registry_get(from);
                     self.registry_set(to, val);
-                },
-                Instruction::Compare(a, b, result) => {
+                }
+                INST_COMPARE => {
+                    let a = self.read_registry()?;
+                    let b = self.read_registry()?;
+                    let result = self.read_registry()?;
+
                     let a_val = self.registry_get(a);
                     let b_val = self.registry_get(b);
                     let result_val = if a_val == b_val {
@@ -95,44 +151,45 @@ impl Emulator {
                         1
                     };
                     self.registry_set(result, result_val);
-                },
-            }
-            self.instruction_pointer += 1;
+                }
+                INST_JUMP => {
+                    let addr = self.read_address()?;
+                    self.jump(addr);
+                }
+                INST_JUMP_IF_NON_ZERO => {
+                    let reg = self.read_registry()?;
+                    let addr = self.read_address()?;
+
+                    let val = self.registry_get(reg);
+                    if val != 0 {
+                        self.jump(addr);
+                    }
+                }
+                INST_PRINT => {
+                    let reg = self.read_registry()?;
+
+                    let val = self.registry_get(reg);
+                    println!("{}", val);
+                }
+                _ => return Err(EmulatorError::InvalidInstruction(inst)),
+            };
         }
+        Ok(())
+    }
+
+    fn read_address(&mut self) -> Result<u32, EmulatorError> {
+        Ok(self.bytes.read_u32::<BigEndian>()?)
     }
 
     fn jump(&mut self, to: u32) {
-        self.instruction_pointer = to as usize - 1;
+        self.bytes.set_position(to as u64);
     }
 
-    fn registry_get(&self, registry: Registry) -> i32 {
+    fn registry_get(&self, registry: u8) -> i32 {
         self.registries.get(&registry).unwrap_or(&0).clone()
     }
 
-    fn registry_set(&mut self, registry: Registry, val: i32) {
+    fn registry_set(&mut self, registry: u8, val: i32) {
         self.registries.insert(registry, val);
     }
-}
-
-
-#[derive(Debug, Hash, PartialEq, Eq, Copy, Clone)]
-enum Registry {
-    Stack,
-    A,
-    B,
-    C,
-    D,
-    E,
-}
-
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
-enum Instruction {
-    LoadConstant(Registry, i32),
-    Add(Registry, Registry, Registry),
-    Subtract(Registry, Registry, Registry),
-    Print(Registry),
-    Jump(u32),
-    JumpIfNonZero(Registry, u32),
-    Copy(Registry, Registry),
-    Compare(Registry, Registry, Registry),
 }
